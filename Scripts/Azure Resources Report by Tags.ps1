c29a3201b33af4f650e0ffbc0d92839ae252004e
Aman Sharma <20aman@gmail.com>
2016-10-07 16:59:29 -0400

    Get Azure Resources Report by Tags
    Script for Getting Azure Resources Report by Tags in Azure

---
+++ Scripts/Azure Resources Report by Tags.ps1
@@ -0,0 +1,50 @@
+﻿#Adding Azure Account and Subscription
+Add-AzureRmAccount
+
+#Selecting the Azure Subscription
+Select-AzureRmSubscription -SubscriptionName "Your Subscription Name Here"
+
+#Getting all Azure Resources
+$resources = Get-AzureRmResource
+
+#Declaring Variables
+$results = @()
+$TagsAsString = ""
+
+foreach($resource in $resources)
+{
+    #Fetching Tags
+    $Tags = $resource.Tags
+    
+    #Checkign if tags is null or have value
+    if($Tags -ne $null)
+    {
+        foreach($Tag in $Tags)
+        {
+            $TagsAsString += $Tag.Name + ":" + $Tag.Value + ";"
+        }
+    }
+    else
+    {
+        $TagsAsString = "NULL"
+    }
+
+    #Adding to Results
+    $details = @{            
+                Tags = $TagsAsString
+                Name = $resource.Name
+                ResourceId = $resource.ResourceId
+                ResourceName = $resource.ResourceName
+                ResourceType = $resource.ResourceType
+                ResourceGroupName =$resource.ResourceGroupName
+                Location = $resource.Location
+                SubscriptionId = $resource.SubscriptionId 
+                Sku = $resource.Sku
+        }                           
+        $results += New-Object PSObject -Property $details 
+
+    #Clearing Variable
+    $TagsAsString = ""
+}
+
+$results | export-csv -Path "C:\DATA\Projects\Accudyne\Resources By Tags\Tags-Accudyne-Operations-Production.csv" -NoTypeInformation
