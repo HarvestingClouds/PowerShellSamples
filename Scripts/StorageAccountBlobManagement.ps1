d8ef8914524b23501a4ecb6385ff3a690afdf461
Aman Sharma <20aman@gmail.com>
2020-04-08 00:34:53 -0400

    Updated to the newer commands

--- Scripts/StorageAccountBlobManagement.ps1
+++ Scripts/StorageAccountBlobManagement.ps1
@@ -1,8 +1,9 @@
-﻿#The below line will prompt for your 
-Login-AzureRmAccount
+#The below line will prompt for your credentials to login into Azure
+Add-AzAccount
 
 #Selecting the subscription if you have more than one subscriptions in your Azure account
-Select-AzureRmSubscription -SubscriptionId "1aa1aaa1-111-11a1-1a1e-bdf5f12e61db"
+$context = Get-AzSubscription -SubscriptionId "1aa1aaa1-111-11a1-1a1e-bdf5f12e61db"
+Set-AzContext $context
 
 $StorageAccountName = "armtemplatesrepo"
 $ResourceGroupNameOfStorage = "ARMTemplatesRG"
@@ -10,8 +11,8 @@ $ContainerName = "armtemplatesblob"
 
 #Set the Context in one of the 3 ways as per your security requirements
 #Ref: - https://azure.microsoft.com/en-us/documentation/articles/storage-powershell-guide-full/
-$StorageAccountKey = Get-AzureRmStorageAccountKey -StorageAccountName $StorageAccountName -ResourceGroupName $ResourceGroupNameOfStorage
-$Ctx = New-AzureStorageContext $StorageAccountName -StorageAccountKey $StorageAccountKey[0].Value
+$StorageAccountKey = Get-AzStorageAccountKey -AccountName $StorageAccountName -ResourceGroupName $ResourceGroupNameOfStorage
+$Ctx = New-AzStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $StorageAccountKey[0].Value
 
 
 #Uploading File
@@ -19,11 +20,11 @@ $BlobName = "Parameters.json"
 $localFile = "D:\Local\" + $BlobName
 
 #Note the Force switch will overwrite if the file already exists in the Azure container
-Set-AzureStorageBlobContent -File $localFile -Container $ContainerName -Blob $BlobName -Context $Ctx -Force
+Set-AzStorageBlobContent -File $localFile -Container $ContainerName -Blob $BlobName -Context $Ctx -Force
 
 
 #Download File
 $BlobName = "Parameters.json"
 $localTargetDirectory = "D:\DownloadedFile"
 
-Get-AzureStorageBlobContent -Blob $BlobName -Container $ContainerName -Destination $localTargetDirectory -Context $ctx
+Get-AzStorageBlobContent -Blob $BlobName -Container $ContainerName -Destination $localTargetDirectory -Context $ctx
