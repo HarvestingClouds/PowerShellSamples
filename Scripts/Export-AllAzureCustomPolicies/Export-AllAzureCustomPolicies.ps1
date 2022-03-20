6df51ef728ce6c93427d074a0f746d806b7eb21a
Aman Sharma <20aman@gmail.com>
2022-03-20 11:06:26 -0400

    Create Export-AllAzureCustomPolicies.ps1

---
+++ Scripts/Export-AllAzureCustomPolicies/Export-AllAzureCustomPolicies.ps1
@@ -0,0 +1,28 @@
+#Logging into Azure
+$env = Get-AzEnvironment -Name "AzureCloud"
+Connect-AzAccount -Environment $env
+
+#Getting all Subscriptions in your environment
+$allSubscriptions = Get-AzSubscription
+
+#Iterating through subscriptions
+foreach($currentSubscription in $allSubscriptions)
+{
+    #setting the context to the current subscription
+    Set-AzContext -SubscriptionName $currentSubscription.Name
+
+    #Getting all the custom policies
+    $policies = Get-AzPolicyDefinition | where {$_.Properties.PolicyType -eq 'Custom'}
+    
+    #Iterating through the policies
+    foreach ($policy in $policies) {
+        
+        #Getting the policy details
+        $policyName = $policy.Name
+        $fileName = $currentSubscription.Name + "_" + $policyName + ".json"
+
+        #Exporting the current custom policy
+        $policy | ConvertTo-Json -Depth 10 | Out-File ".\Export-AzurePolicies\Output\$fileName"
+    }
+
+}
